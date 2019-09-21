8669fd9651ec39a4250717843193002c23b181e0
fork52 <mrunankmistry52@gmail.com>
2019-09-21 21:32:17 +0530

    Generating successfully

--- mazeGen.py
+++ mazeGen.py
@@ -13,7 +13,7 @@ WALL_WIDTH = 2 			#WALL WIDTH IN PIXELS
 GAP_WIDTH  = 10
 noOfWalls  = 25
 
-visited = [ [0 for i in range(noOfWalls-1)],[0 for i in range(noOfWalls-1)] ]
+visited = [ [0 for i in range(noOfWalls-1)] for i in range(noOfWalls-1) ]
 BLACK   = (  0,   0,   0)
 WHITE	= (255, 255, 255)
 
@@ -24,7 +24,7 @@ mousex , mousey = 0 , 0
 FPS = 20
 
 def main():
-	global FPSCLOCK,DISPLAYSURF,WALL,visited
+	global FPSCLOCK,DISPLAYSURF,WALL,visited,DISPLAY_HEIGHT,DISPLAY_HEIGHT
 	os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (300, 50)
 
 
@@ -37,6 +37,7 @@ def main():
 	FPSCLOCK = pygame.time.Clock()
 	DISPLAYSURF.fill(WHITE)
 
+	#DRAW THE GRID
 	WALL=0
 	while(WALL<=DISPLAY_WIDTH-1):
 		pygame.draw.line(DISPLAYSURF,BLACK,(WALL,0),(WALL,DISPLAY_HEIGHT),1)
@@ -47,11 +48,8 @@ def main():
 
 
 
-	xtemp1,ytemp1,xtemp2,ytemp2 = findWallCoordinates(23,0,2)
-	colorCoordinates(xtemp1,ytemp1,2)
-	colorCoordinates(xtemp2,ytemp2,2)
 
-	print(visited)
+	# print(visited)
 
 	while True:
 		for event in pygame.event.get():
@@ -59,7 +57,7 @@ def main():
 				pygame.image.save(DISPLAYSURF,'maze1.png')
 				pygame.quit()
 				sys.exit()
-
+		recursiveBacktracker(0,0)
 		pygame.display.update()
 		FPSCLOCK.tick(FPS)
 
@@ -101,24 +99,70 @@ def colorCoordinates(xS,yS,code):
 	elif code==2 or code==4:	#left or right
 		pygame.draw.line(DISPLAYSURF,WHITE,(xS,yS),(xS,yS+GAP_WIDTH-1),1)
 
-def recursiceBacktracker(x,y):
-	visited[x][y]=1
+def checkIfvisisted(x1,y1,code):
+	"Checks if the upper/lower/right/left tile of x1,y1 is visited or not"
+	global  visisted
+	if code ==1:
+		return visited[x1][y1-1]==0
+	elif code ==2:
+		return visited[x1+1][y1]==0
+	elif code ==3:
+		return visited[x1][y1+1]==0
+	elif code ==4:
+		return visited[x1-1][y1]==0
+
+def nextCoordinates(x1,y1,code):
+	"Returns the co-ordinates of the next tile to be visited depending on the code passed"
+	if code ==1:
+		return x1,y1-1
+	elif code ==2:
+		return x1+1,y1
+	elif code ==3:
+		return x1,y1+1
+	elif code ==4:
+		return x1-1,y1
+
+def recursiveBacktracker(x1,y1):
+	""" It is assumed that the current tile x1,y1 is unvisited when u enter the code"""
+	global visited
+	visited[x1][y1]=1 #mark it visisted
+
 
 	l=[] #wallChoicesAvaliable
-	if y!=0:
+	#generate available choices
+	if y1!=0:
 		l.append(1)
-	if x!=23:
+	if x1!=23:
 		l.append(2)
-	if y!=23:
+	if y1!=23:
 		l.append(3)
-	if x!=0:
+	if x1!=0:
 		l.append(4)
+
+
 	print(l)
-
 	wallCode=random.choice(l)
-	
 
+	#remove the wall corresponding to the wallCode obtained
+	xtemp1,ytemp1,xtemp2,ytemp2 = findWallCoordinates(x1,y1,wallCode)
+	colorCoordinates(xtemp1,ytemp1,wallCode)
+	colorCoordinates(xtemp2,ytemp2,wallCode)
 
+	for i in l:
+		isNotVisited = checkIfvisisted(x1,y1,i)
+		if not isNotVisited:
+			l.remove(i)
+
+	print(x1,y1,l)
+	if l==[]:
+		return
+
+	random.shuffle(l)
+	for j in l:
+		newX ,newY = nextCoordinates(x1,y1,j)
+		if visited[newX][newY]==1:
+			return
+		recursiveBacktracker(newX,newY)
 
 if __name__ == '__main__':
     main()
